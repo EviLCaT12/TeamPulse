762aa34611a871364a87bb50b4f14642bdefb072
Тимофей Кузнецов <smile_kito@mail.ru>
2025-05-09 16:33:08 +1000

    add preparation for working with errors

--- mainService/src/Shared/TeamPulse.SharedKernel/Errors/Error.cs
+++ mainService/src/Shared/TeamPulse.SharedKernel/Errors/Error.cs
@@ -2,5 +2,33 @@ namespace TeamPulse.SharedKernel.Errors;
 
 public class Error
 {
+    private Error(string message, string code, ErrorType errorType)
+    {
+        ErrorMessage = message;
+        ErrorCode = code;
+        Type = errorType;
+    }
+    
+    public string ErrorCode { get; init; } 
+    
+    public string ErrorMessage { get; init; }
+    
+    public ErrorType Type { get; init; }
+
+    public static Error Validation(string message, string code) =>
+        new(message, code, ErrorType.Validation);
+    
+    public static Error NotFound(string message, string code) =>
+        new(message, code, ErrorType.NotFound);
+    
+    public static Error Failure(string message, string code) =>
+        new(message, code, ErrorType.Failure);
+    
+    public enum ErrorType
+    {
+        Validation,
+        NotFound,
+        Failure
+    }
     
 }
---
+++ mainService/src/Shared/TeamPulse.SharedKernel/Errors/ErrorList.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+
+namespace TeamPulse.SharedKernel.Errors;
+
+public class ErrorList(IEnumerable<Error> errors) : IEnumerable<Error>
+{
+    private readonly List<Error> _errors = [..errors];
+    public IEnumerator<Error> GetEnumerator()
+    {
+        return _errors.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
