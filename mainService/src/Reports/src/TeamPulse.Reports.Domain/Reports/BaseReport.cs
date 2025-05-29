507413ba3c775e389b52fa0b106ff974cbbf53f8
Тимофей Кузнецов <smile_kito@mail.ru>
2025-05-30 01:02:05 +1000

    add reports module, add median value report

---
+++ mainService/src/Reports/src/TeamPulse.Reports.Domain/Reports/BaseReport.cs
@@ -0,0 +1,34 @@
+using CSharpFunctionalExtensions;
+using TeamPulse.SharedKernel.Errors;
+using TeamPulse.SharedKernel.SharedVO;
+
+namespace TeamPulse.Reports.Domain.Reports;
+
+public abstract class BaseReport : Entity<ReportId>
+{
+    //ef core
+    private BaseReport() {}
+
+    protected BaseReport(
+        ReportId id,
+        Guid departmentId,
+        Guid teamId,
+        Name name,
+        Description description) : base(id)
+    {
+        DepartmentId = departmentId;
+        TeamId = teamId;
+        Name = name;
+        Description = description;
+    }
+    
+    public Guid DepartmentId { get; protected set; }
+    
+    public Guid TeamId { get; protected set; }
+    
+    public Name Name { get; protected set; } = null!;
+    
+    public Description Description { get; protected set; } = null!;
+    
+    public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
+}
---
+++ mainService/src/Reports/src/TeamPulse.Reports.Domain/Reports/MedianValueReport.cs
@@ -0,0 +1,89 @@
+using CSharpFunctionalExtensions;
+using TeamPulse.SharedKernel.Errors;
+using TeamPulse.SharedKernel.SharedVO;
+
+namespace TeamPulse.Reports.Domain.Reports;
+
+/// <summary>
+/// Отчёт, дающий информацию о среднем показателе владения той или иной группой(команда/отдел)
+/// какой-либо группой скиллов
+/// </summary>
+/// <typeparam name="TGradeType">Тип оценки</typeparam> 
+/// <typeparam name="TMedianType">Тип среднего арифметического(для int) и моды (для string)</typeparam> 
+public class MedianValueReport<TGradeType, TMedianType> : BaseReport, IGradeReports<TGradeType>
+{
+    private MedianValueReport(
+        ReportId reportId,
+        Guid departmentId,
+        Guid teamId,
+        Name name,
+        Description description,
+        Dictionary<Guid, TGradeType> gradesByEmployees,
+        TMedianType medianValue) : base(reportId, departmentId, teamId, name, description)
+    {
+        GradeByEmployee = gradesByEmployees;
+        MedianValue = medianValue;
+    }
+
+    public Dictionary<Guid, TGradeType> GradeByEmployee { get; private set; }
+    public TMedianType MedianValue { get; private set; }
+
+
+    public Result<BaseReport, Error> CreateReport(BaseReport report, Dictionary<Guid, TGradeType> gradeByEmployee)
+    {
+        if (gradeByEmployee.Count == 0)
+            return Errors.General.ValueIsRequired("Employees and grades cannot be empty.");
+
+        if (typeof(TGradeType) == typeof(int))
+        {
+            decimal totalSum = gradeByEmployee.Values
+                .Aggregate(0, (current, grade) => current + (int)(object)grade!);
+            
+            var medianValue = totalSum / gradeByEmployee.Count;
+
+            var intGrades = gradeByEmployee.ToDictionary(
+                kvp => kvp.Key,
+                kvp => (int)(object)kvp.Value!);
+            
+            return new MedianValueReport<int, decimal>(
+                report.Id,
+                report.DepartmentId,
+                report.TeamId,
+                report.Name,
+                report.Description,
+                intGrades,
+                medianValue);
+        }
+
+        if (typeof(TGradeType) == typeof(string))
+        {
+            var stringGrades = gradeByEmployee.Values.Cast<string>().ToList();
+
+            var mostFrequentString = stringGrades
+                .GroupBy(s => s)
+                .OrderByDescending(s => s.Count())
+                .Select(g => g.Key)
+                .ToList();
+            
+            if (mostFrequentString.Count > 1)
+                return Errors.General.ValueIsInvalid("More than mod frequent strings are not supported.");
+
+            var medianValue = mostFrequentString.First();
+            
+            var stringGradesDict = gradeByEmployee.ToDictionary(
+                kvp => kvp.Key,
+                kvp => (string)(object)kvp.Value!);
+            
+            return new MedianValueReport<string, string>(
+                report.Id,
+                report.DepartmentId,
+                report.TeamId,
+                report.Name,
+                report.Description,
+                stringGradesDict,
+                medianValue);
+        }
+        
+        return Errors.General.ValueIsInvalid("Unknown grade type.");
+    }
+}
